d9e2ecaef52adfc36e328091868077d648bdc1bb
AR Shahin <56718222+AR-Shahin@users.noreply.github.com>
2020-03-02 23:45:11 +0600

    main file

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#bar1').barfiller({barColor: "#f1c40f"});
+    $('#bar2').barfiller({barColor: "#e67e22"});
+    $('#bar3').barfiller({barColor: "#9b59b6"});
+    $('#bar4').barfiller({barColor: "#990000"});
+    $('#bar5').barfiller({barColor: "#3498db"});
+    $('#bar6').barfiller({barColor: "#fdcb6e"});
+    $('#bar7').barfiller({barColor: "#00cec9"});
+    $('#bar8').barfiller({barColor: "#990000"});
+    $('#bar9').barfiller({barColor: "#c23616"});
+    $('#bar10').barfiller({barColor: "#718093"});
+    $('#bar11').barfiller({barColor: "#4cd137"});
+     var containerEl = document.querySelector('.container');
+
+            var mixer = mixitup(containerEl);
+});
